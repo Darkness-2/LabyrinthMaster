61ec92d8a8eb407d5a35ad7a019a030738a5c88a
Justin Moulton <justinmmm690@gmail.com>
2013-05-24 13:57:13 -0400

    First Commit

---
+++ GlobalVars.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        LabyrinthMaster - GlobalVars
+# Purpose:     To store global variables.
+#
+# Author:      Justin Moulton
+#
+# Created:     May 24, 2013
+# Copyright:   (c) Justin Moulton 2013
+# Licence:     This work is licensed under the Creative Commons
+#              Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+#              To view a copy of this license, visit
+#              http://creativecommons.org/licenses/by-nc-sa/3.0/.
+#-------------------------------------------------------------------------------
+
+#Colours------------------------------------------------------------------------
+aqua = (0, 255, 255)
+black = (  0,   0,   0)
+blue = (  0,  0, 255)
+fuchsia = (255,   0, 255)
+gray = (128, 128, 128)
+green = (  0, 128,   0)
+lime = (  0, 255,   0)
+maroon = (128,  0,   0)
+navyBlue = (  0,  0, 128)
+olive = (128, 128,   0)
+purple = (128,  0, 128)
+red = (255,   0,   0)
+silver = (192, 192, 192)
+teal = (  0, 128, 128)
+white = (255, 255, 255)
+yellow = (255, 255,   0)
---
+++ LabyrinthMaster.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Name:        LabyrinthMaster
+# Purpose:     Main file of LabyrinthMaster.
+#
+# Author:      Justin Moulton
+#
+# Created:     May 24, 2013
+# Copyright:   (c) Justin Moulton 2013
+# Licence:     This work is licensed under the Creative Commons
+#              Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+#              To view a copy of this license, visit
+#              http://creativecommons.org/licenses/by-nc-sa/3.0/.
+#-------------------------------------------------------------------------------
+
+import jLibrary
+import GlobalVars
+import pygame
+from pygame.locals import *
+import sys
+
+def main():
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((700, 500))
+    pygame.display.set_caption('Labyrinth Master')
+    DISPLAYSURF.fill(GlobalVars.white)
+    spamRect = pygame.Rect(10, 20, 200, 300)
+    pygame.draw.rect(DISPLAYSURF, GlobalVars.blue, spamRect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()
